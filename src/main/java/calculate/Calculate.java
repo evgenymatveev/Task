39a3778e98601709134c7a63d2d83ee0b866fa73
apple <apple@Air-apple-2.Dlink>
2016-10-15 00:23:31 +0300

    add second Calculate.java

---
+++ src/main/java/calculate/Calculate.java
@@ -0,0 +1,34 @@
+package calculate;
+
+
+public class Calculate {
+
+	private double result;
+	
+	public void add(double first, double second){
+
+		this.result = first+second;
+
+	}
+
+	public void substruct(double first, double second){
+
+		this.result = first-second;
+
+	}
+
+	public void div(double first, double second){
+
+		
+		this.result = first/second;
+	
+	}
+
+
+	public void multiple(double first, double second){
+
+		this.result = first*second;
+
+	}
+
+}
