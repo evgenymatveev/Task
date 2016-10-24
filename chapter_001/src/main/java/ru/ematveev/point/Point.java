91f3c1c7325478ddcef0655d17440c3f58a55700
apple <apple@Air-apple-2.Dlink>
2016-10-24 22:24:05 +0300

    change file Point.java

--- chapter_001/src/main/java/ru/ematveev/point/Point.java
+++ chapter_001/src/main/java/ru/ematveev/point/Point.java
@@ -28,50 +28,7 @@ import java.lang.Math.*;
 
 }
 	
-public class Triangle {
-			public Point a;
-			public Point b;
-			public Point c;
-
-	public Triangle(Point a, Point b, Point c) {
-			this.a = a;
-			this.b = b;
-			this.c = c;
-	}
-
-	public double area() {
-		//calculate the triangle area
-
-				double ab = a.distanceTo(b);
-				double bc = b.distanceTo(c);
-				double ca = c.distanceTo(a);
-
-				double d = (ab + bc + ca)/2;
-				double s = Math.sqrt(d*(d-ab)*(d-bc)*(d-ca));
-
-				if (s > 0)
-
-					return s;
-
-				else 
-
-					return -1;
-		}
-			
-}
-
-public class Main{
-					
-	public static void main ( String[] args ){
-
-			Point a = new Point();
-			Point b = new Point();
-			Point c = new Point();
-
-			double area = new Triangle(a, b, c).area();
 
 
-	}
-		
-}
+
 
