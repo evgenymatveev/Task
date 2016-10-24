ad093f626ff1f4f25fd067cbff9b8c8f10e7ea26
apple <apple@Air-apple-2.Dlink>
2016-10-24 22:25:11 +0300

    add file Triangle.java

---
+++ chapter_001/src/main/java/ru/ematveev/point/Triangle.java
@@ -0,0 +1,36 @@
+package ru.ematveev.point;
+
+import java.lang.Math.*;
+
+
+public class Triangle {
+			public Point a;
+			public Point b;
+			public Point c;
+
+	public Triangle(Point a, Point b, Point c) {
+			this.a = a;
+			this.b = b;
+			this.c = c;
+	}
+
+	public double area() {
+		//calculate the triangle area
+
+				double ab = a.distanceTo(b);
+				double bc = b.distanceTo(c);
+				double ca = c.distanceTo(a);
+
+				double d = (ab + bc + ca)/2;
+				double s = Math.sqrt(d*(d-ab)*(d-bc)*(d-ca));
+
+				if (s > 0)
+
+					return s;
+
+				else 
+
+					return -1;
+		}
+			
+}
