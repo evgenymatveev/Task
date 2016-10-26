cd46cac32a5f2c8eadb5cffafab2cd8ebcbd92b3
apple <apple@Air-apple-2.Dlink>
2016-10-26 16:14:11 +0300

    add MaxValue.java

---
+++ chapter_001/src/main/java/ru/ematveev/point/MaxValue.java
@@ -0,0 +1,30 @@
+package ru.ematveev.point;
+
+public class MaxValue{
+
+		public double ab;
+		public double bc;
+		public double ca;
+
+		public MaxValue(double ab, double bc, double ca){
+
+			this.ab = ab;
+			this.bc = bc;
+			this.ca = ca;
+		}
+
+	public double max(){
+
+			double max = ab;
+
+				if(bc > max && bc > ca)
+
+					max = bc;
+	
+				else max = ca;
+ 
+		return max;
+
+}
+
+}
