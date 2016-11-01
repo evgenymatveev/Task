494820b266750d536508fc1655fe349a0def50c1
apple <apple@Air-apple-2.Dlink>
2016-11-02 01:31:05 +0300

    add file Factorial.java

---
+++ chapter_001/src/main/java/ru/ematveev/factorial/Factorial.java
@@ -0,0 +1,32 @@
+package ru.ematveev.factorial;
+
+public class Factorial{
+	
+	public int n;
+
+	public Factorial(int n){
+
+		this.n = n;
+
+	}
+
+	public int factorial(){
+
+				int f = 1;
+
+					if(n > 1){
+
+						for (int i = 1; i < n; i++){
+
+				f = f * (i + 1);	
+			
+		}
+
+	}
+
+return f;
+
+	}
+
+
+}
