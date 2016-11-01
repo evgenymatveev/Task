c67c1fbfbc58cb8666c982d1e0c1982a613bfe58
apple <apple@Air-apple-2.Dlink>
2016-11-01 11:40:37 +0300

    add file Square.java

---
+++ chapter_001/src/main/java/ru/ematveev/square/Square.java
@@ -0,0 +1,38 @@
+package ru.ematveev.square;
+
+public class Square{
+
+	public int a;
+	public int b;
+	public int c;
+
+	public Square(int a, int b, int c){
+
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	public double calculate(int x){
+
+		double y = a * x * x + b * x + c;
+
+		return y;
+
+	}
+
+	public void show(int start, int finish, int step){
+
+
+			for(int i = 0; i <= finish; i = i + step){
+
+				System.out.println(calculate(start));
+
+				start = start + step;
+
+		}
+
+	}
+}
+
+		
