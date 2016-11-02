69439cf15baa56efd84085139f08f7db3318718b
apple <apple@MacBook-Air-apple-2.local>
2016-11-02 13:16:51 +0300

    change Squaretest.java

--- chapter_001/src/test/java/ru/ematveev/square/SquareTest.java
+++ chapter_001/src/test/java/ru/ematveev/square/SquareTest.java
@@ -10,13 +10,8 @@ public class SquareTest{
 
 	@Test
 	public void calculate(){
-
 		Square square = new Square(2, 3, 4);
-		
 		double result = square.calculate(2);
-
 		assertThat(result, closeTo(18d, 0.001));
-
 	}
-
 }
