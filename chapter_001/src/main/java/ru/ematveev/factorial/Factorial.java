4277d52e4a0974840d7e3a91ea4c4f0c8b0e0c1f
apple <apple@MacBook-Air-apple-2.local>
2016-11-02 14:01:17 +0300

    change Factorial.java

--- chapter_001/src/main/java/ru/ematveev/factorial/Factorial.java
+++ chapter_001/src/main/java/ru/ematveev/factorial/Factorial.java
@@ -5,28 +5,18 @@ public class Factorial{
 	public int n;
 
 	public Factorial(int n){
-
 		this.n = n;
-
 	}
 
 	public int factorial(){
-
 				int f = 1;
-
 					if(n > 1){
-
 						for (int i = 1; i < n; i++){
-
-				f = f * (i + 1);	
-			
+				f = f * (i + 1);
+			}
 		}
-
-	}
 
 return f;
 
 	}
-
-
 }
