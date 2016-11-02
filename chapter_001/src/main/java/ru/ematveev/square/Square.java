0c46fd79ce62c2caf3f86c0ab9bcba9d918c23d3
apple <apple@MacBook-Air-apple-2.local>
2016-11-02 13:20:31 +0300

    change Square.java

--- chapter_001/src/main/java/ru/ematveev/square/Square.java
+++ chapter_001/src/main/java/ru/ematveev/square/Square.java
@@ -1,37 +1,25 @@
 package ru.ematveev.square;
 
 public class Square{
-
 	public int a;
 	public int b;
 	public int c;
 
 	public Square(int a, int b, int c){
-
 		this.a = a;
 		this.b = b;
 		this.c = c;
 	}
 
 	public double calculate(int x){
-
-		double y = a * x * x + b * x + c;
-
-		return y;
-
+		return a * x * x + b * x + c;;
 	}
 
 	public void show(int start, int finish, int step){
-
-
 			for(int i = 0; i <= finish; i = i + step){
-
 				System.out.println(calculate(start));
-
 				start = start + step;
-
 		}
-
 	}
 }
 
