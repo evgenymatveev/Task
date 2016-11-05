5caa8ef319884b1aa068f0f596f16fe9ef45705d
apple <apple@Air-apple-2.Dlink>
2016-11-05 20:05:42 +0300

    add ArraySort.java

---
+++ chapter_001/src/main/java/ru/ematveev/arraysort/ArraySort.java
@@ -0,0 +1,28 @@
+package ru.ematveev.arraysort;
+
+public class ArraySort{
+
+	  public int [] arraySort(int[] array){
+	  	for(int i = 0; i < array.length; i++){
+	  		int min = array[i];
+	  		int index = i;
+	  	
+	  		for(int j = i+1; j < array.length; j++){
+	  			if(array[j] < min){
+	  				min = array[j];
+	  				index = j;
+	  	}
+	 }
+				if(i != index){
+	  				int tmp = array[i];
+	  				array[i] = array[index];
+	  				array[index] = tmp;
+	  		}		 	
+	  		
+	 	}
+
+	 	return array;
+	 			
+	}
+
+}
