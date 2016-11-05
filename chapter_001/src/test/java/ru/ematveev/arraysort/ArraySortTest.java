69a79ef3569c87e5e71b542c9ab6c0a1d823f7f8
apple <apple@Air-apple-2.Dlink>
2016-11-05 20:11:47 +0300

    git add ArraySortTest.java

---
+++ chapter_001/src/test/java/ru/ematveev/arraysort/ArraySortTest.java
@@ -0,0 +1,15 @@
+package ru.ematveev.arraysort;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+
+public class ArraySortTest{
+		@Test
+		public void arraySortTest(){
+			ArraySort array = new ArraySort();
+			int [] arr = new int[]{23, 8, 5, 9, 2};;
+			assertThat((array.arraySort(arr)), is(new int[]{2, 5, 8, 9, 23}));
+		}
+}
