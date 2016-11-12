ed521d2cafb143d0210230f070c08835d6d7853a
apple <apple@MacBook-Air-apple-2.local>
2016-11-12 18:47:35 +0300

    change ArrayDublicate.java

--- chapter_001/src/main/java/ru/ematveev/arraydublicate/ArrayDublicate.java
+++ chapter_001/src/main/java/ru/ematveev/arraydublicate/ArrayDublicate.java
@@ -4,9 +4,10 @@ public class ArrayDublicate{
 
 	public String [] arrayDublicate(String [] array){
 
-
-			int x = array.length;    
-			
+			int x = array.length;
+			int p = array.length;
+			int a = 0;
+			  		
 			for(int i = 0; i < x; i++){
 					
 				for(int j = 1; j < x; j++){
@@ -16,16 +17,21 @@ public class ArrayDublicate{
 						if(array[i].equals(array[j+i])){
 							array[i] = null;
 							}				 				
-					
 					}
-
 							x--;
 				}
-					return array;
+					for(int k = 0; k < p; k++){
+						if(array[k] != null){				
+							array[a] = array[k];
+							array[k] = null;
+							a++;			
+						}		
+					}
+
+				return array;
 	}
 }
 
+	
 
-
-
-
+							
