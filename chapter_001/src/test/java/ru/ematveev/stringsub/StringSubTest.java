86dc75e19747af1812addeb2472db8cceb585c4b
apple <apple@Air-apple-2.Dlink>
2016-11-15 16:52:54 +0300

    add StringSubTest.java

---
+++ chapter_001/src/test/java/ru/ematveev/stringsub/StringSubTest.java
@@ -0,0 +1,17 @@
+package ru.ematveev.stringsub;
+
+
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+
+
+public class StringSubTest{
+	@Test
+	public void containsTest(){
+		StringSub d = new StringSub();
+		assertThat(d.contains("Светлана", "ветла"), is(true));
+	}
+}
