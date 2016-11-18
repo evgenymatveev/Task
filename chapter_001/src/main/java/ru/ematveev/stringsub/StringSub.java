7402bc125ae3cf66aa047478e3897f9c0b14dc2b
apple <apple@Air-apple-2.Dlink>
2016-11-18 19:35:54 +0300

    change StringSub.java

--- chapter_001/src/main/java/ru/ematveev/stringsub/StringSub.java
+++ chapter_001/src/main/java/ru/ematveev/stringsub/StringSub.java
@@ -5,17 +5,17 @@ public class StringSub{
 		public boolean contains(String origin, String sub){
 
 			char[] o = origin.toCharArray();
-			char[] s = origin.toCharArray();
+			char[] s = sub.toCharArray();
 			boolean count = false;
 
 				for(int i = 0; i < o.length; i++){
 					if(s[0] == o[i]){
 						for(int j = 1; j < s.length; j++){
-							if(s[j] != o[i+j]){
-							break;
+							if(s[j] == o[i+j]){
+							count = true;
 						}
 							else {
-							count = true;
+							count = false;
 					}
 				}
 			}
