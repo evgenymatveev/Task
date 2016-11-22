84bf12129f2c35b64e38bcf4635acd10863452ea
apple <apple@Air-apple-2.Dlink>
2016-11-22 19:27:52 +0300

    add task mergearray

---
+++ chapter_001/src/main/java/ru/ematveev/mergesort/MergeSort.java
@@ -0,0 +1,32 @@
+package ru.ematveev.mergesort;
+
+public class MergeSort{
+	
+	public int[] mergeSort(int[] a, int[] b){
+
+		int[] c = new int[a.length + b.length];
+		int i = 0;
+		int j = 0;
+		int k = 0;
+
+		while(i < a.length && j < b.length){	
+				if (a[i] < b[j]){
+				c[k++] = a[i++];
+				}
+				
+				else {
+				c[k++] = b[j++];
+				}
+		}				
+		for(;i < a.length;){
+			c[k++] = a[i++];
+		}
+		for(;j < b.length;){
+			c[k++] = b[j++];
+		}
+				return c;
+	}
+}
+
+
+
