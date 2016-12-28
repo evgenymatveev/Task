76a3528ec59e0d583c78eefd9837d8f5f833f3ce
evgenymatveev <evgeni2007@yandex.ru>
2016-12-28 18:53:53 +0300

    Bug - add comment

---
+++ chapter_002/src/main/java/ru/ematveev/model/Bug.java
@@ -0,0 +1,19 @@
+package ru.ematveev.model;
+
+/**
+ * Сlass Bug for create the item with out enter option .
+ * @author Matveev Evgeny.
+ * @version 1.0.
+ * @since 25.12.2016.
+ */
+public class Bug extends Item {
+    /**
+     * Comstructor for Bug.
+     * @param name name.
+     * @param desc description.
+     * @param create create.
+     */
+    public Bug(String name, String desc, long create) {
+        super(name, desc, create);
+    }
+}
