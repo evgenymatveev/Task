40621f7c014895855375aa29347cd34e28d1535d
evgenymatveev <evgeni2007@yandex.ru>
2017-01-08 03:24:40 +0300

    UpdateItem - create

--- chapter_002/src/main/java/ru/ematveev/model/MenuTracker.java
+++ chapter_002/src/main/java/ru/ematveev/model/MenuTracker.java
@@ -5,6 +5,39 @@ import ru.ematveev.start.Tracker;
 import ru.ematveev.start.UserAction;
 
 /**
+ * Class UpdateItem - implement the interface UserAction for update the item.
+ */
+class UpdateItem implements UserAction {
+        /**
+         * The method returns the number key.
+         * @return number 2.
+         */
+        public int key() {
+            return 2;
+        }
+        /**
+         * The method prompting the user information of the application and update the item.
+         * @param input input.
+         * @param tracker tracker.
+         */
+        public void execute(Input input, Tracker tracker) {
+            String id = input.ask("Please, enter the task's id: ");
+            String name = input.ask("Please, enter the task's name: ");
+            String desc = input.ask("Please, enter the task's desc: ");
+            Task task = new Task(name, desc);
+            task.setId(id);
+            tracker.update(task.getId(), task);
+        }
+        /**
+         * Method for output on the screen menu items.
+         * @return String.
+         */
+        public String info() {
+            return String.format("%s. %s", this.key(), "Edit the item.");
+        }
+    }
+
+/**
  * Class for create menu.
  *
  * @author Matveev Evgeny.
@@ -15,7 +48,7 @@ public class MenuTracker {
     /**
      * The number of menu items.
      */
-    private final int x = 5;
+    private final int x = 6;
     /**
      * Variable for input information from the user.
      */
@@ -43,7 +76,12 @@ public class MenuTracker {
      * Method recording the task in the array.
      */
     public void fillAction() {
-        this.actions[0] = new AddItem();
+        this.actions[0] = this.new AddItem();
+        this.actions[1] = new MenuTracker.ShowItems();
+        this.actions[2] = new UpdateItem();
+        this.actions[3] = this.new DeleteItem();
+        this.actions[4] = new FindByNameItem();
+        this.actions[5] = new FindByIdItem();
     }
 
     /**
@@ -71,7 +109,7 @@ public class MenuTracker {
     private class AddItem implements UserAction {
         /**
          * The method returns the number key.
-         * @return 0.
+         * @return number 0.
          */
         public int key() {
             return 0;
@@ -96,4 +134,155 @@ public class MenuTracker {
             return String.format("%s. %s", this.key(), "Add the new item.");
         }
     }
+
+    /**
+     * Class ShowItems - implement the interface UserAction for show the items.
+     */
+    private static class ShowItems implements UserAction {
+        /**
+         * The method returns the number key.
+         * @return number 1.
+         */
+        public int key() {
+            return 1;
+        }
+
+        /**
+         * The method displays on screen all the items.
+         * @param input input.
+         * @param tracker tracker.
+         */
+        public void execute(Input input, Tracker tracker) {
+            for (Item item : tracker.findAll()) {
+                System.out.println(
+                        String.format("%s. %s", item.getId(), item.getName())
+                );
+            }
+        }
+
+        /**
+         * Method for output on the screen all the items.
+         * @return String.
+         */
+        public String info() {
+            return String.format("%s. %s", this.key(), "Show all items.");
+        }
+    }
+
+    /**
+     * Class DeleteItem - implement the interface UserAction for delete the item.
+     */
+    private class DeleteItem implements UserAction {
+        /**
+         * Number key for memu.
+         */
+        private final int countKey = 3;
+        /**
+         * The method returns the number key.
+         * @return number 2.
+         */
+        public int key() {
+            return countKey;
+        }
+
+        /**
+         * The method prompting the user information of the application and delete item.
+         * @param input input.
+         * @param tracker tracker.
+         */
+        public void execute(Input input, Tracker tracker) {
+            String id = input.ask("Please, enter the task's id: ");
+            String name = input.ask("Please, enter the task's name: ");
+            String desc = input.ask("Please, enter the task's desc: ");
+            Task task = new Task(name, desc);
+            task.setId(id);
+            tracker.delete(task);
+        }
+
+        /**
+         * Method for output on the screen menu items.
+         * @return String.
+         */
+        public String info() {
+            return String.format("%s. %s", this.key(), "Delete the item.");
+        }
+    }
+
+    /**
+     * Class findByNameItem - implement the interface UserAction for search the item named.
+     */
+    private class FindByNameItem implements UserAction {
+        /**
+         * Number key for memu.
+         */
+        private final int countKey = 4;
+        /**
+         * The method returns the number key.
+         * @return number 4.
+         */
+        public int key() {
+            return countKey;
+        }
+
+        /**
+         * The method search for an item named.
+         * @param input input.
+         * @param tracker tracker.
+         */
+        public void execute(Input input, Tracker tracker) {
+            String name = input.ask("Please, enter the task's name: ");
+            Item item = tracker.findByName(name);
+            System.out.println(
+                    String.format("%s, %s, %s", item.getId(), item.getName(), item.getDescription())
+            );
+        }
+
+        /**
+         * Method for output on the screen menu items.
+         * @return String.
+         */
+        public String info() {
+            return String.format("%s. %s", this.key(), "Search the item named.");
+        }
+    }
+
+    /**
+     * Class findByIdItem - implement the interface UserAction for search the item id.
+     */
+    private class FindByIdItem implements UserAction {
+        /**
+         * Number key for memu.
+         */
+        private final int countKey = 5;
+        /**
+         * The method returns the number key.
+         * @return number 5.
+         */
+        public int key() {
+            return countKey;
+        }
+
+        /**
+         * The method search for an item named.
+         * @param input input.
+         * @param tracker tracker.
+         */
+        public void execute(Input input, Tracker tracker) {
+            String id = input.ask("Please, enter the task's id: ");
+            Item item = tracker.findById(id);
+            System.out.println(
+                    String.format("%s, %s, %s", item.getId(), item.getName(), item.getDescription())
+            );
+        }
+
+        /**
+         * Method for output on the screen menu items.
+         * @return String.
+         */
+        public String info() {
+            return String.format("%s. %s", this.key(), "Search the item Id.");
+        }
+    }
+
+
 }
