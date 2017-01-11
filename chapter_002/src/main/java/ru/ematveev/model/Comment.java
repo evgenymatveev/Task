89ed4276af9f8f42a1d94154eb2f0ef49f8f0cb3
evgenymatveev <evgeni2007@yandex.ru>
2017-01-11 17:25:10 +0300

    Commite - add arrayOutComment.

---
+++ chapter_002/src/main/java/ru/ematveev/model/Comment.java
@@ -0,0 +1,44 @@
+package ru.ematveev.model;
+
+/**
+ * Class Comment for create comments for items.
+ *
+ * @author Matveev Evgeny.
+ * @version 1.0.
+ * @since 09.01.17.
+ */
+public class Comment {
+    /**
+     * The number of possible coment in the array.
+     */
+    private int count = 10;
+    /**
+     * The aray for storage the comments.
+     */
+    private String[] arrayComment = new String[count];
+    /**
+     * The number of comments.
+     */
+    private int position = 0;
+
+    /**
+     * Method for adding the comments.
+     * @param comment comment.
+     */
+    public void addComments(String comment) {
+        this.arrayComment[position++] = comment;
+    }
+
+    /**
+     * Method for the output to the console.
+     */
+    public void arrayOutComment() {
+        for (String comment : arrayComment) {
+            if (comment != null) {
+                System.out.println(comment);
+            } else {
+                break;
+            }
+        }
+    }
+}
