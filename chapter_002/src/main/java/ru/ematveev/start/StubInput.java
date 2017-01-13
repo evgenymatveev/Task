2df149f54edc061f2bebb341f640bb68a471d77a
evgenymatveev <evgeni2007@yandex.ru>
2017-01-13 20:37:55 +0300

    StabInput - create.

---
+++ chapter_002/src/main/java/ru/ematveev/start/StubInput.java
@@ -0,0 +1,37 @@
+package ru.ematveev.start;
+
+/**
+ * Class.
+ *
+ * @author Matveev Evgeny.
+ * @version 1.0.
+ * @since 03.01.17.
+ */
+public class StubInput implements Input {
+    /**
+     * Array the answers.
+     */
+    private String[] answers;
+
+    /**
+     * Count of answers.
+     */
+    private int position = 0;
+
+    /**
+     * Constructor.
+     * @param answers answers.
+     */
+    public StubInput(String[] answers) {
+        this.answers = answers;
+    }
+
+    /**
+     * implementation of class Input.
+     * @param question question
+     * @return answer.
+     */
+    public String ask(String question) {
+        return answers[position++];
+    }
+}
