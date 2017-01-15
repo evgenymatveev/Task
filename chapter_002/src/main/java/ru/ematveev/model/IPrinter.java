439ddb42621f75a9040f42c240526551cb7ae389
evgenymatveev <evgeni2007@yandex.ru>
2017-01-15 04:32:15 +0300

    IPrinter - create interface IPrinter for testing.

---
+++ chapter_002/src/main/java/ru/ematveev/model/IPrinter.java
@@ -0,0 +1,12 @@
+package ru.ematveev.model;
+
+/**
+ * Interface IPrinter.
+ */
+public interface IPrinter {
+    /**
+     * Method for realization.
+     * @param text text.
+     */
+    void println(String text);
+}
