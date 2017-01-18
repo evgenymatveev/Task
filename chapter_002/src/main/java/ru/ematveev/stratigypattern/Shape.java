368d6074b6dc5f6bf0682a7b5540346a23812d45
evgenymatveev <evgeni2007@yandex.ru>
2017-01-18 17:56:17 +0300

    Shape - create

---
+++ chapter_002/src/main/java/ru/ematveev/stratigypattern/Shape.java
@@ -0,0 +1,16 @@
+package ru.ematveev.stratigypattern;
+
+/**
+ * Interface Shape (Strategy).
+ *
+ * @author Matveev Evgeny.
+ * @version 1.0.
+ * @since 18.01.17.
+ */
+public interface Shape {
+    /**
+     * The base method to create the figures.
+     * @return String.
+     */
+    String pic();
+}
