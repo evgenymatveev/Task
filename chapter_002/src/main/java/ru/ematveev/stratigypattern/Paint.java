c85a20525e7e73c8f0d23b83721cd4a3c5774d15
evgenymatveev <evgeni2007@yandex.ru>
2017-01-18 19:39:18 +0300

    Paint - checkstyle

--- chapter_002/src/main/java/ru/ematveev/stratigypattern/Paint.java
+++ chapter_002/src/main/java/ru/ematveev/stratigypattern/Paint.java
@@ -1,5 +1,7 @@
 package ru.ematveev.stratigypattern;
 
+import ru.ematveev.model.IPrinter;
+
 /**
  * Class Paint this StratigyClient.
  *
@@ -12,35 +14,49 @@ public class Paint {
      * Interface Shape connection.
      */
     private Shape shape;
+    /**
+     * Iprinter for output to console.
+     */
+    private IPrinter iPrinter;
 
     /**
      * Setter for Shape.
      * @param shape shape.
      */
-    private void setShape(Shape shape) {
+    public void setShape(Shape shape) {
         this.shape = shape;
     }
 
     /**
      * Method that prints to the console figures.
+     * @param iPrinter iPrinter.
      */
-    public void draw() {
-        System.out.println(shape.pic());
+    public void draw(IPrinter iPrinter) {
+        iPrinter.println(shape.pic());
     }
 
     /**
      * Method start the program.
      * @param args args.
      */
+
     public static void main(String[] args) {
+        IPrinter iPrinter = new IPrinter() {
+            @Override
+            public void println(String text) {
+                System.out.println(text);
+            }
+        };
         Paint paint = new Paint();
 
         QuadreShape quadreShape = new QuadreShape();
         paint.setShape(quadreShape);
-        paint.draw();
+        paint.draw(iPrinter);
 
         TriangleShape triangleShape = new TriangleShape();
         paint.setShape(triangleShape);
-        paint.draw();
+        paint.draw(iPrinter);
+
+
     }
 }
