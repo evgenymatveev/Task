93c5e35fa60b8b7540187a49e405e7605fb5b48b
evgenymatveev <evgeni2007@yandex.ru>
2017-01-19 14:09:03 +0300

    QuadreShape - checkstyle

--- chapter_002/src/main/java/ru/ematveev/stratigypattern/QuadreShape.java
+++ chapter_002/src/main/java/ru/ematveev/stratigypattern/QuadreShape.java
@@ -1,6 +1,5 @@
 package ru.ematveev.stratigypattern;
 
-
 /**
  * Class QuadreShape implements Shape drawing a square.
  *
@@ -9,18 +8,39 @@ package ru.ematveev.stratigypattern;
  * @since 18.01.17.
  */
 public class QuadreShape implements Shape {
+//    private int n;
+//
+//    public QuadreShape(int n) {
+//        this.n = n;
+//    }
+
     /**
      * Method pic() create square.
+     *
      * @return String;
      */
-    public String pic() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("\n").append(" __________")
-                .append("\n").append("|          |")
-                .append("\n").append("|          |")
-                .append("\n").append("|          |")
-                .append("\n").append("|          |")
-                .append("\n").append("|__________|");
-        return sb.toString();
+    public String pic(int n) {
+        String result = "";
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == (n - 1) || j == 0 || j == (n - 1)) {
+//                    System.out.print("x");
+//                    System.out.print(" ");
+                    result = result + "x";
+                    result = result + " ";
+                } else {
+//                    System.out.print("@ ");
+                    result = result + "@ ";
+                }
+            }
+//            System.out.println();
+            result = result + "\n";
+        }
+        return result;
     }
+
 }
+
+
+
+
