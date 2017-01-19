80d90e604854d284ca1b2d723f87ff7e7511f2c2
evgenymatveev <evgeni2007@yandex.ru>
2017-01-19 14:09:41 +0300

    TriangleShape - checkstyle

--- chapter_002/src/main/java/ru/ematveev/stratigypattern/TriangleShape.java
+++ chapter_002/src/main/java/ru/ematveev/stratigypattern/TriangleShape.java
@@ -10,16 +10,58 @@ package ru.ematveev.stratigypattern;
 public class TriangleShape implements Shape {
     /**
      * Method pic() create triangle.
+     *
      * @return String;
      */
-    public String pic() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("     /\\")
-                .append("\n").append("    /  \\")
-                .append("\n").append("   /    \\")
-                .append("\n").append("  /      \\")
-                .append("\n").append(" /        \\")
-                .append("\n").append("/__________\\");
-        return sb.toString();
+    public String pic(int n) {
+        String result = "";
+        int x = 0;
+        for (int i = n; i > 0; i--) {
+            for (int j = i; j > 0; j--) {
+                //System.out.print(" ");
+                result = result + " ";
+            }
+//            System.out.print("/");
+//            System.out.print(generateBackSlash(x) + "\\" + "\n");
+
+            result = result + "/";
+            result = result + generateBackSlash(x) + "\\" + "\n";
+            x = x + 2;
+        }
+//        System.out.print("/");
+//        System.out.print(generateSeparator(x, n) + "\\");
+        result = result + "/";
+        result = result + generateSeparator(x, n) + "\\";
+        return result;
+    }
+
+    /**
+     * Method for generete baskslash.
+     * @param x x.
+     * @return String.
+     */
+    public String generateBackSlash(int x) {
+        String a = "";
+        for (int h = x; h > 0; h--) {
+            //System.out.print("@");
+            a = a + "@";
+        }
+        return a;
+    }
+
+    /**
+     * Method generate separator.
+     * @param x x.
+     * @param n n.
+     * @return String.
+     */
+    public String generateSeparator(int x, int n) {
+        String a = "";
+        for (int h = n; h > 0; h--) {
+            //System.out.print("__");
+            a = a + "__";
+        }
+        return a;
     }
 }
+
