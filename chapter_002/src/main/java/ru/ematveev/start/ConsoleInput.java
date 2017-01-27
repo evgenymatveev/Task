49b6a26eb8ef538aef9caf99cd721e348565806a
evgenymatveev <evgeni2007@yandex.ru>
2017-01-27 11:26:46 +0300

    ConsoleInput - add MenuOutException

--- chapter_002/src/main/java/ru/ematveev/start/ConsoleInput.java
+++ chapter_002/src/main/java/ru/ematveev/start/ConsoleInput.java
@@ -10,14 +10,30 @@ import java.util.Scanner;
  * @since 03.01.17.
  */
 public class ConsoleInput implements Input {
+
+    private Scanner scanner = new Scanner(System.in);
     /**
      * Method for the questions to the user.
      * @param question question
      * @return String
      */
     public String ask(String question) {
-        Scanner scanner = new Scanner(System.in);
         System.out.print(question);
         return scanner.nextLine();
+    }
+    public int ask(String question, int[] ranges)  throws MenuOutException {
+        int key = Integer.valueOf(this.ask(question));
+        boolean exist = false;
+        for(int value : ranges){
+            if (value == key){
+                exist = true;
+                break;
+            }
+        }
+        if (exist) {
+            return key;
+        } else {
+            throw new MenuOutException("Out of menu range");
+        }
     }
 }
