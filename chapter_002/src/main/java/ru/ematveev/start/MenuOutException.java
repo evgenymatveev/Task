5c620cf484195c1164f077283b9d2dd4f3c4c61c
evgenymatveev <evgeni2007@yandex.ru>
2017-01-27 11:28:15 +0300

    MenuOutException - create

---
+++ chapter_002/src/main/java/ru/ematveev/start/MenuOutException.java
@@ -0,0 +1,14 @@
+package ru.ematveev.start;
+
+/**
+ * Class
+ *
+ * @author Matveev Evgeny.
+ * @version 1.0.
+ * @since 26.01.17.
+ */
+public class MenuOutException extends Exception{
+    public MenuOutException(String msg) {
+        super(msg);
+    }
+}
