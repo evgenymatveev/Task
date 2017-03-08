e817a67c563af97fe74c2170bf60baacf88ce484
evgenymatveev <evgeni2007@yandex.ru>
2017-03-08 22:02:09 +0300

    ArrayOne - checkstyle.

--- tasks/src/main/java/ArrayOne.java
+++ tasks/src/main/java/ArrayOne.java
@@ -1,8 +1,16 @@
 import java.util.Arrays;
 
+/**
+ * Class removes duplicates in the array (int[]).
+ */
 public class ArrayOne {
     private static int index = 0;
 
+    /**
+     * Method removes duplicates in the array (int[]).
+     * @param arr array number int.
+     * @return array int with out dublicate.
+     */
     private int[] arrayDeleteDublicate(int[] arr) {
         int x = arr.length;
             for (int i = 0, j = 1; j < x; i++, j++) {
@@ -21,6 +29,12 @@ public class ArrayOne {
         return arr;
     }
 
+    /**
+     * Method checks the sequence number matches.
+     * @param n next number coinciding with previous.
+     * @param arr array number int.
+     * @return next index of element of array which not the same as the previous.
+     */
     private int checked(int n, int[] arr) {
         int result = n + 1;
             for (int k = n + 1; k < arr.length; k++) {
@@ -32,12 +46,22 @@ public class ArrayOne {
         return result;
     }
 
+    /**
+     * Method change elements in the array, transferring the duplicates to the end of the array.
+     * @param a element which will replace next element.
+     * @param b next element.
+     * @param arr array number int.
+     */
     private void change(int a, int b, int[] arr) {
         for (int i = a, j = b; j < arr.length; i++, j++) {
             arr[i] = arr[j];
         }
     }
 
+    /**
+     * Method main for start the program.
+     * @param args array String arguments.
+     */
     public static void main(String[] args) {
         ArrayOne arrayOne = new ArrayOne();
         int[] arr = new int[]{10, 11, 33, 5, 5, 5, 8, 2, 2, 7, 7, 9, 11, 12, 2, 2, 2, 3, 3, 3, 6};
