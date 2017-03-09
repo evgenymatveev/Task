a7486eb7b53ae81084aec903e4e355403b835449
evgenymatveev <evgeni2007@yandex.ru>
2017-03-09 19:04:56 +0300

    ByteInputStream - create

---
+++ chapter_003/src/main/java/ByteInputStream.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+/**
+ * @author Matveev Evgeny.
+ */
+public class ByteInputStream {
+    /**
+     * The method checks that the byte stream written is an even number.
+     * @param in the input stream.
+     * @return booleat true if number is even and false if number is odd.
+     */
+    public boolean isNumber(InputStream in) {
+        boolean result = false;
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
+            int x = Integer.parseInt(br.readLine());
+                if ((x % 2) == 0 || x == 0) {
+                    result = true;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        return result;
+    }
+}
