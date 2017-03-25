72339c5fca3650cc672058a113d42913c434b99f
evgenymatveev <evgeni2007@yandex.ru>
2017-03-25 23:43:33 +0300

    CheckedByteEven - checkstyle

--- chapter_003/src/main/java/ru/ematveev/CheckedByteEven.java
+++ chapter_003/src/main/java/ru/ematveev/CheckedByteEven.java
@@ -1,30 +1,31 @@
 package ru.ematveev;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.IOException;
 
 /**
  * @author Matveev Evgeny.
  */
-public class CheckedByteEven {
-    /**
-     * The method checks that the byte stream written is an even number.
-     * @param in the input stream.
-     * @return booleat true if number is even and false if number is odd.
-     */
-    public boolean isNumber(InputStream in) {
-        boolean result = false;
+    public class CheckedByteEven {
+        /**
+         * The method checks that the byte stream written is an even number.
+         * @param in the input stream.
+         * @return booleat true if number is even and false if number is odd.
+         */
+        public boolean isNumber(InputStream in) {
+            boolean result = false;
 
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
-            int x = Integer.parseInt(br.readLine());
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
+                int x = Integer.parseInt(br.readLine());
                 if ((x % 2) == 0) {
                     result = true;
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        return result;
+            return result;
+        }
     }
-}
+
