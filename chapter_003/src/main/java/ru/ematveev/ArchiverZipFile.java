833060d3d6fe674af4b1ae160440ab64581763c4
evgenymatveev <evgeni2007@yandex.ru>
2017-03-26 17:19:55 +0300

    Add method init().

--- chapter_003/src/main/java/ru/ematveev/ArchiverZipFile.java
+++ chapter_003/src/main/java/ru/ematveev/ArchiverZipFile.java
@@ -1,9 +1,6 @@
 package ru.ematveev;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.zip.Deflater;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -16,10 +13,10 @@ public class ArchiverZipFile {
      * The file which needs to be eventually archived.
      */
     private final File arhZip;
-    /**
-     * Directory from which to read files.
-     */
-    private final File inDir;
+//    /**
+//     * Directory from which to read files.
+//     */
+//    private final File inDir;
     /**
      * The list of extentions.
      */
@@ -28,33 +25,33 @@ public class ArchiverZipFile {
     /**
      * The class that produces the archiving of files.
      * @param arhZip the file which needs to be eventually archived.
-     * @param inDir the list of extentions.
+     //* @param inDir the list of extentions.
      * @param ext the switch for the method checkFile() checking extensions.
      */
-    public ArchiverZipFile(File arhZip, File inDir, String[] ext) {
+    public ArchiverZipFile(File arhZip, String[] ext) {
         this.arhZip = arhZip;
-        this.inDir = inDir;
+        //this.inDir = inDir;
         this.ext = ext;
     }
 
     /**
      * The method reviewer is to read the object directory or file.
      */
-    public void checkDirectory() {
+    public void checkDirectory(File in, ZipOutputStream z) {
 
-            if (inDir.isDirectory()) {
-                File[] list = inDir.listFiles();
-                assert list != null;
+        if (in.isDirectory()) {
+            File[] list = in.listFiles();
+            assert list != null;
                 for (File item : list) {
-                        if (item.isDirectory()) {
-                            checkDirectory();
-                        } else {
-                                if (checkFile(item)) {
-                                    arh(item);
-                                }
+                    if (item.isDirectory()) {
+                        checkDirectory(item, z);
+                    } else {
+                        if (checkFile(item)) {
+                            arh(item, z);
                         }
                     }
-            }
+                }
+        }
     }
 
     /**
@@ -94,34 +91,44 @@ public class ArchiverZipFile {
         File in = new File(args[3]);
         String[] s = args[5].split(",");
 
-        System.out.println(args[1]);
-
-        ArchiverZipFile archiverZipFile = new ArchiverZipFile(outZip, in, s);
-        archiverZipFile.checkDirectory();
+        ArchiverZipFile archiverZipFile = new ArchiverZipFile(outZip, s);
+            try {
+                archiverZipFile.init(in);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
     }
+    public void init(File in) throws FileNotFoundException {
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(arhZip))){
+            checkDirectory(in, zipOutputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
     /**
      * Writes files into the archive.
      * @param in the file to write to the archive.
      */
-    private void arh(File in) {
+    private void arh(File in, ZipOutputStream z) {
 
-        try (ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(arhZip));
-             FileInputStream fileInputStream = new FileInputStream(in);) {
-                byte[] buffer = new byte[1024];
-                zipOutputStream.setLevel(Deflater.DEFAULT_COMPRESSION);
-                ZipEntry zipEntry = new ZipEntry(in.getName());
-                zipOutputStream.putNextEntry(zipEntry);
-                int length;
-                    while ((length = fileInputStream.read(buffer)) > -1) {
-                        zipOutputStream.write(buffer, 0, length);
-                    }
-                zipOutputStream.closeEntry();
+        try(FileInputStream fileInputStream = new FileInputStream(in)) {
+
+            byte[] buffer = new byte[1024];
+            z.setLevel(Deflater.DEFAULT_COMPRESSION);
+            ZipEntry zipEntry = new ZipEntry(in.getName());
+            z.putNextEntry(zipEntry);
+            int length;
+                while ((length = fileInputStream.read(buffer)) > -1) {
+                    z.write(buffer, 0, length);
+                    z.closeEntry();
+                }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
-
 
