07bf6dd43f6f0c755345b17df15efcfc3146b552
evgenymatveev <evgeni2007@yandex.ru>
2017-06-08 20:35:28 +0300

    IteratorEvenNumbersTest - create

---
+++ chapter_005/src/test/java/ru/ematveev/iterator/IteratorEvenNumbersTest.java
@@ -0,0 +1,48 @@
+package ru.ematveev.iterator;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Matveev Evgeny.
+ */
+public class IteratorEvenNumbersTest {
+    /**
+     * Method check method next().
+     */
+    @Test
+    public void whenCallMethodNext() {
+        IteratorEvenNumbers it = new IteratorEvenNumbers();
+
+        it.next();
+        it.next();
+        it.next();
+        it.next();
+        int result = (int) it.next();
+
+        assertThat(result, is(10));
+    }
+
+    /**
+     * Method check method hasNext().
+     */
+    @Test
+    public void whenCheckPositionWhenCallHasNext() {
+        IteratorEvenNumbers it = new IteratorEvenNumbers();
+
+        it.next();
+        it.next();
+        it.next();
+        it.next();
+        it.next();
+        it.next();
+        it.next();
+        it.next();
+        it.hasNext();
+        boolean result = it.hasNext();
+
+        assertThat(result, is(false));
+    }
+}
