7d366f95ccd9b4be0e75606e18c6b8772b08ca9e
evgenymatveev <evgeni2007@yandex.ru>
2017-06-18 20:13:50 +0300

    Role - create

---
+++ chapter_005/src/main/java/ru/ematveev/generic/Role.java
@@ -0,0 +1,20 @@
+package ru.ematveev.generic;
+
+/**
+ * @author Matveev Evgeny.
+ */
+public class Role extends Base {
+    public Role(String id) {
+        super(id);
+    }
+
+    @Override
+    public String getId() {
+        return super.getId();
+    }
+
+    @Override
+    public void setId(String id) {
+        super.setId(id);
+    }
+}
