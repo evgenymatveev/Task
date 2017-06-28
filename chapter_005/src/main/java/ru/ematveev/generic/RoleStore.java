b75173395f52318b1360a5390f0bfc2efbb0485f
evgenymatveev <evgeni2007@yandex.ru>
2017-06-28 19:18:13 +0300

    5.2.2. Реализовать Store<T extends Base>[#17223]

--- chapter_005/src/main/java/ru/ematveev/generic/RoleStore.java
+++ chapter_005/src/main/java/ru/ematveev/generic/RoleStore.java
@@ -4,23 +4,23 @@ package ru.ematveev.generic;
  * @author Matveev Evgeny.
  */
 public class RoleStore extends AbstractStore {
-    @Override
-    public void add(Base value) {
-        super.add(value);
-    }
-
-    @Override
-    public void update(Base valueOld, Base valueNew) {
-        super.update(valueOld, valueNew);
-    }
-
-    @Override
-    public void delete(Base value) {
-        super.delete(value);
-    }
-
-    @Override
-    public SimpleArray getBase() {
-        return super.getBase();
-    }
+//    @Override
+//    public void add(Base value) {
+//        super.add(value);
+//    }
+//
+//    @Override
+//    public void update(Base valueOld, Base valueNew) {
+//        super.update(valueOld, valueNew);
+//    }
+//
+//    @Override
+//    public void delete(Base value) {
+//        super.delete(value);
+//    }
+//
+//    @Override
+//    public SimpleArray getBase() {
+//        return super.getBase();
+//    }
 }
--- chapter_005/src/main/java/ru/ematveev/generic/UserStore.java
+++ chapter_005/src/main/java/ru/ematveev/generic/UserStore.java
@@ -4,24 +4,24 @@ package ru.ematveev.generic;
  * @author Matveev Evgeny.
  */
 public class UserStore extends AbstractStore {
-    @Override
-    public void add(Base value) {
-        super.add(value);
-    }
-
-    @Override
-    public void update(Base valueOld, Base valueNew) {
-        super.update(valueOld, valueNew);
-    }
-
-    @Override
-    public void delete(Base value) {
-        super.delete(value);
-    }
-
-    @Override
-    public SimpleArray getBase() {
-        return super.getBase();
-    }
+//    @Override
+//    public void add(Base value) {
+//        super.add(value);
+//    }
+//
+//    @Override
+//    public void update(Base valueOld, Base valueNew) {
+//        super.update(valueOld, valueNew);
+//    }
+//
+//    @Override
+//    public void delete(Base value) {
+//        super.delete(value);
+//    }
+//
+//    @Override
+//    public SimpleArray getBase() {
+//        return super.getBase();
+//    }
 }
 
