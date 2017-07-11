6d9d6885cfa51cff27943f15a6fb3daaee7ce8e8
evgenymatveev <evgeni2007@yandex.ru>
2017-07-11 21:55:57 +0300

    2. Не перекрывать equals hashCode [#1005]

---
+++ chapter_005/src/test/java/ru/ematveev/map/UserTest.java
@@ -0,0 +1,31 @@
+package ru.ematveev.map;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Tests the map.
+ */
+public class UserTest {
+    /**
+     * Test the map when the methods equals and hashcode is not redefined.
+     */
+    @Test
+    public void whenMethodsEqualsAndHashcodeNotRedefined() {
+        Calendar birthdayfirst = new GregorianCalendar(1977, 5, 7);
+        Calendar birthdaysecond = new GregorianCalendar(1977, 5, 7);
+        User first = new User("Evgen", 30, birthdayfirst);
+        User second = new User("Evgen", 30, birthdaysecond);
+
+        Map<User, Object> map = new HashMap<>();
+        map.put(first, null);
+        map.put(second, null);
+
+        System.out.println(map);
+    }
+
+}
