99fdef8ece083feece3fef49f4436f04329503c9
evgenymatveev <evgeni2007@yandex.ru>
2017-07-11 21:55:22 +0300

    1. Создать модель User [#999]

---
+++ chapter_005/src/main/java/ru/ematveev/map/User.java
@@ -0,0 +1,43 @@
+package ru.ematveev.map;
+
+
+import java.util.Calendar;
+
+/**
+ * The model users.
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
+}
